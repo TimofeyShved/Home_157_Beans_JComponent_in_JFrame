0d9ca89c914b53fd6e6f246ae07972b9e601642a
TimofeyShved <timofeya123@inbox.ru>
2023-12-28 23:13:21 +0600

    Create bean

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Можно и в ручную создать и отрисовать, форму и компонент (кастомный)
+        JFrame jFrame = getFrame();
+        FaceBean faceBean = new FaceBean();
+        faceBean.setMouthWidth(50);
+        faceBean.frown();
+        jFrame.add(faceBean);
+    }
+
+    // что бы не делать портянку вынесли JFrame
+    static JFrame getFrame(){
+        // сама форма
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // размеры
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width/2-250, dimension.height/2-250, 500,500);
+
+        return jFrame;
     }
 }
